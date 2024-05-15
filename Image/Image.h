a5b2dbfb142af27d8281ac5bad474c1d6f4df00e
Arseni Salavei <arsenisoloveii@gmail.com>
2024-05-15 14:40:16 +0200

    created a basic structure for the project

---
+++ Image/Image.h
@@ -0,0 +1,11 @@
+#include <string>
+#ifndef IMAGE_STEGANOGRAPHY_IMAGE_H
+#define IMAGE_STEGANOGRAPHY_IMAGE_H
+
+
+class Image {
+
+};
+
+
+#endif //IMAGE_STEGANOGRAPHY_IMAGE_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <fmt/core.h>
+
+auto main(int argc, char *argv[]) -> int {
+    fmt::println("Hello, world!");
+}
