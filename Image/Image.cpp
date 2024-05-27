67933c277fbabc5348c638f565ce1d8791340415
Arseni Salavei <arsenisoloveii@gmail.com>
2024-05-27 21:46:01 +0200

    created a basic structure for the project image class declaration

--- Image/Image.cpp
+++ Image/Image.cpp
@@ -1,1 +1,18 @@
 #include "Image.h"
+
+
+Image::Image(std::string const& filepath) {
+    // todo
+//    auto path = std::string(filepath);
+//    auto size = std::filesystem::file_size(path);
+//    auto image = std::fstream(path, std::ios::in | std::ios::binary);
+//    auto bytes = std::vector<char>(size);
+//
+//    image.read(bytes.data(), bytes.size());
+
+}
+
+auto Image::getSize() const -> unsigned int { return p_size; }
+auto Image::getDepth() const -> unsigned int { return p_depth; }
+auto Image::getWidth() const -> unsigned int { return p_width; }
+auto Image::getHeight() const -> unsigned int { return p_height; }
--- Image/Image.h
+++ Image/Image.h
@@ -1,11 +1,34 @@
-#include <string>
 #ifndef IMAGE_STEGANOGRAPHY_IMAGE_H
 #define IMAGE_STEGANOGRAPHY_IMAGE_H
-
+#include <string>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+#include "../utils/core.cpp"
 
 class Image {
+private:
+    std::vector<char> p_data;
+    unsigned int p_size=0;
+    unsigned int p_width;
+    unsigned int p_height;
+    unsigned int p_depth;                // number of channels
 
+public:
+    Image(std::string const& filepath);
+    Image(Image const& img);
+    ~Image() = default;
+
+    // getters
+    auto getWidth() const -> unsigned int;
+    auto getHeight() const -> unsigned int;
+    auto getDepth() const -> unsigned int;
+    auto getSize() const -> unsigned int;
+
+    // methods
+    auto check(std::string const& message) const -> bool;
+    auto encrypt(std::string const& message) -> void;
+    auto decrypt() const -> std::string;
 };
 
-
-#endif //IMAGE_STEGANOGRAPHY_IMAGE_H
+#endif
