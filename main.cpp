8b22f87b334adb9f5cb8335806cdbe2fd4b08d82
Arseni Salavei <arsenisoloveii@gmail.com>
2024-06-11 19:32:01 +0200

    created a basic structure for the project image class declaration core implementation main execution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <fmt/core.h>
-#include "utils/core.h"
+#include "core/core.h"
 
 auto main(int argc, char *argv[]) -> int {
-    execute();
+    switch (argc) {
+        case 4:
+            execute(argv[1], argv[2], argv[3]);
+            break;
+        case 3:
+            execute(argv[1], argv[2]);
+            break;
+        case 2:
+            execute(argv[1]);
+            break;
+        case 1:
+            execute();
+            break;
+        default:
+            throw std::invalid_argument("Invalid number of arguments, try -help, -h to know more about the program.");
+    }
+
 }
